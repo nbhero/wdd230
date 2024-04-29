c904ba6019a9b9c0811dcc2ccd2dde3cee0bc44f
Bruno Rafael <brunorafael72@outlook.com>
2024-04-29 00:16:21 -0300

    Mobile View - Test

---
+++ scripts/getDate.js
@@ -0,0 +1,43 @@
+const apiKey = "a209ea9d26b2c89a9c0c101eebef022c";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city) {    
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    const weatherIcon = document.querySelector(".weather-icon")
+
+    if (response.status == 404) {
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)} °C`;
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".country").innerHTML = data.sys['country'];
+
+        if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/sunny_white.png"
+        }
+        else if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/cloudy_white.png"
+        }
+        else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rainny_white.png"
+        }
+        else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist_white.png"
+        }
+        else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle_white.png"
+        }
+
+        document.querySelector(".weather").style.display = "block";
+    }
+}
+
+checkWeather("Franca")
+
+let lastModif = document.lastModified
+document.querySelector("#lastModified").innerHTML = `Last Modification: ${lastModif}`;
+const year = new Date
+document.querySelector(".year").innerHTML = year.getFullYear();
