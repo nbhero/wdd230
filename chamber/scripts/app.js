5748975e42c28540c6a75092f2016c09d7afaf4e
unknown <brunorafael72@outlook.com>
2024-05-12 12:49:43 -0300

    Update

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -4,6 +4,48 @@ modeIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 5
 const menuButton = document.querySelector('#hamburguer-menu');
 const navMenu = document.querySelector(".nav-menu");
 
+const visitsDisplay = document.querySelector(".visits");
+let numberOfVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+// Trying the function
+function getVisitMessage() {
+    const lastVisit = localStorage.getItem("lastVisit");
+    const msToDays = 84600000;
+    if (!lastVisit) {
+        return "Welcome";
+    }
+
+    const lastVisitDate = new Date(lastVisit);
+    const now = new Date();
+    const deltaDays = Math.floor((now - lastVisitDate) / msToDays);
+    console.log(deltaDays)
+
+    if (deltaDays === 0) {
+        return "Back so soon? Awesome!";
+    }
+    else {
+        const message = `You last visited ${deltaDays} day${deltaDays > 1 ? 's' : ''}`;
+        return message;
+    }
+}
+
+function setLastVisit() {
+    localStorage.setItem("lastVisit", new Date().toISOString());
+}
+const visitMessage = document.querySelector("#visit-message");
+visitMessage.textContent = getVisitMessage();
+setLastVisit();
+// End of Code
+if (numberOfVisits !== 0) {
+    visitsDisplay.textContent = numberOfVisits;
+}
+else {
+    visitsDisplay.textContent = `Welcome to California's Chamber of Commerce!`;
+}
+
+numberOfVisits++;
+localStorage.setItem('numVisits-ls', numberOfVisits);
+
 menuButton.addEventListener('click', () => {
     navMenu.classList.toggle('open');
     menuButton.classList.toggle('open');
